468a9765432cb5139d234184ccac229f1382b371
VennV <hyplex9009@gmail.com>
2026-03-01 00:22:42 +0700

    Fix some errors

--- tests/test_io.php
+++ tests/test_io.php
@@ -8,19 +8,32 @@ use vosaka\foroutines\Thread;
 
 use function vosaka\foroutines\main;
 
-require '../vendor/autoload.php';
+require "../vendor/autoload.php";
+
+class Test
+{
+    public $arr = [];
+    public function __construct()
+    {
+        var_dump("Test class created");
+    }
+}
 
 main(function () {
-    RunBlocking::new(function () {
-        Launch::new(function () {
+    $class = new Test();
+    $class->arr[] = "Hello, World!";
+    RunBlocking::new(function () use ($class) {
+        Launch::new(function () use ($class) {
             Delay::new(2000);
-            var_dump('World1');
-        });
+            var_dump("World1");
+
+            var_dump($class->arr);
+        }, Dispatchers::IO);
         Launch::new(function () {
             Delay::new(1000);
-            var_dump('World2');
+            var_dump("World2");
         });
-        var_dump('Hello,');
+        var_dump("Hello,");
 
         Thread::wait();
     }, Dispatchers::IO);
